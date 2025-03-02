21c9bd4910daf3a760e242a3eaf0f489aea60141
Fred Carella <fred.carella@gmail.com>
2025-03-02 15:58:49 -0500

    commit

--- src/main/java/csd230/lab4/respositories/BookRepository.java
+++ src/main/java/csd230/lab4/respositories/BookRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByIsbn(String isbn);
+    Book findByIsbn(String isbn);
     Book findById(long id);
 
 }
